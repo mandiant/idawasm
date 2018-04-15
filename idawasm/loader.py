0a9ff7cbbb00a17fe5a16b76af43bd5575589492
William Ballenthin <william.ballenthin@fireeye.com>
2018-04-14 22:02:32 -0600

    idawasm: loader: parse code section into functions

--- idawasm/loader.py
+++ idawasm/loader.py
@@ -34,14 +34,91 @@ def accept_file(f, n):
     return "WebAssembly v%d executable" % (0x1)
 
 
+def offset_of(struc, fieldname):
+    p = 0
+    dec_meta = struc.get_decoder_meta()
+    for field in struc.get_meta().fields:
+        if field.name != fieldname:
+            p += dec_meta["lengths"][field.name]
+        else:
+            return p
+    raise KeyError('field not found: ' + fieldname)
+
+
+def size_of(struc, fieldname=None):
+    if fieldname is not None:
+        # size of the given field, by name
+        dec_meta = struc.get_decoder_meta()
+        return dec_meta["lengths"][fieldname]
+    else:
+        # size of the entire given struct
+        return sum(struc.get_decoder_meta()['lengths'].values())
+
+
+import collections
+Field = collections.namedtuple('Field', ['offset', 'name', 'size'])
+
+def get_fields(struc):
+    p = 0
+    dec_meta = struc.get_decoder_meta()
+    for field in struc.get_meta().fields:
+        flen = dec_meta['lengths'][field.name]
+        if flen > 0:
+            yield Field(p, field.name, flen)
+        p += flen
+
+
+def MakeN(addr, size):
+    if size == 1:
+        idc.MakeByte(addr)
+    elif size == 2:
+        idc.MakeWord(addr)
+    elif size == 4:
+        idc.MakeDword(addr)
+    elif size == 8:
+        idc.MakeQword(addr)
+
+
+def load_code_section(section, p):
+    idc.MakeName(p + offset_of(section.data, "id"), "code_id")
+    MakeN(p + offset_of(section.data, "id"), size_of(section.data, "id"))
+
+    ppayload = p + offset_of(section.data, "payload")
+    idc.MakeName(ppayload + offset_of(section.data.payload, "count"), "function_count")
+    MakeN(ppayload + offset_of(section.data.payload, "count"), size_of(section.data.payload, "count"))
+
+    pbodies = ppayload + offset_of(section.data.payload, "bodies")
+    pcur = pbodies
+    for i, body in enumerate(section.data.payload.bodies):
+        fname = 'function_%X' % (i)
+        idc.MakeName(pcur, fname)
+
+        idc.MakeName(pcur + offset_of(body, "local_count"), fname + '_local_count')
+        MakeN(pcur + offset_of(body, "local_count"), size_of(body, 'local_count'))
+
+        if size_of(body, "locals") > 0:
+            idc.MakeName(pcur + offset_of(body, "locals"), fname + '_locals')
+            for j in range(size_of(body, "locals")):
+                idc.MakeByte(pcur + offset_of(body, "locals") + j)
+
+        idc.MakeName(pcur + offset_of(body, "code"), fname + '_code')
+        idc.MakeCode(pcur + offset_of(body, "code"))
+
+        pcur += size_of(body)
+
+
+SECTION_LOADERS = {
+    wasm.wasmtypes.SEC_CODE: load_code_section,
+}
+
+
 def load_file(f, neflags, format):
     f.seek(0x0, os.SEEK_END)
     flen = f.tell()
     f.seek(0x0)
     buf = f.read(flen)
 
-    # TODO: need to provide a wasm processor
-    idaapi.set_processor_type("8086", idaapi.SETPROC_ALL)
+    idaapi.set_processor_type("wasm", idaapi.SETPROC_ALL)
 
     f.seek(0x0)
     f.file2base(0, 0, len(buf), True)
@@ -51,18 +128,26 @@ def load_file(f, neflags, format):
     for i, section in enumerate(sections):
         if i == 0:
             sname = 'header'
-            stype = 'DATA'
         else:
             if section.data.id == 0:
                 # fetch custom name
                 sname = ''
-                stype = 'DATA'
             else:
                 sname = SECTION_NAMES.get(section.data.id, 'unknown')
-                stype = 'CODE'
+
+        if sname != 'header' and section.data.id == wasm.wasmtypes.SEC_CODE:
+            stype = 'CODE'
+        else:
+            stype = 'DATA'
 
         slen = sum(section.data.get_decoder_meta()['lengths'].values())
         idaapi.add_segm(0, p, p + slen, sname, stype)
+
+        if sname != 'header':
+            loader = SECTION_LOADERS.get(section.data.id)
+            if loader is not None:
+                loader(section, p)
+
         p += slen
 
     # magic
