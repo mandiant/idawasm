8f28ba16a355e83a23a58331e46fb51f4881fdea
William Ballenthin <william.ballenthin@fireeye.com>
2018-04-21 15:25:14 -0600

    loader: better section, element names

--- idawasm/loader.py
+++ idawasm/loader.py
@@ -66,6 +66,17 @@ def MakeN(addr, size):
         idc.MakeQword(addr)
 
 
+def get_section(sections, section_id):
+    for i, section in enumerate(sections):
+        if i == 0:
+            continue
+
+        if section.data.id != section_id:
+            continue
+
+        return section
+
+
 def load_code_section(section, p):
     idc.MakeName(p + offset_of(section.data, 'id'), 'code_id')
     MakeN(p + offset_of(section.data, 'id'), size_of(section.data, 'id'))
@@ -78,7 +89,7 @@ def load_code_section(section, p):
     pcur = pbodies
     for i, body in enumerate(section.data.payload.bodies):
         fname = 'function_%X' % (i)
-        idc.MakeName(pcur, fname)
+        idc.MakeName(pcur, fname + '_meta')
 
         idc.MakeName(pcur + offset_of(body, 'local_count'), fname + '_local_count')
         MakeN(pcur + offset_of(body, 'local_count'), size_of(body, 'local_count'))
@@ -88,15 +99,87 @@ def load_code_section(section, p):
             for j in range(size_of(body, 'locals')):
                 idc.MakeByte(pcur + offset_of(body, 'locals') + j)
 
-        idc.MakeName(pcur + offset_of(body, 'code'), fname + '_code')
-        idc.MakeCode(pcur + offset_of(body, 'code'))
+        pcode = pcur + offset_of(body, 'code')
+        idc.MakeName(pcode, fname)
+        idc.MakeCode(pcode)
+        idc.MakeFunction(pcode)
 
         pcur += size_of(body)
 
 
+def load_globals_section(section, p):
+    idc.MakeName(p + offset_of(section.data, 'id'), 'globals_id')
+    MakeN(p + offset_of(section.data, 'id'), size_of(section.data, 'id'))
+
+    idc.MakeName(p + offset_of(section.data, 'payload_len'), 'globals_size')
+    MakeN(p + offset_of(section.data, 'payload_len'), size_of(section.data, 'payload_len'))
+
+    ppayload = p + offset_of(section.data, 'payload')
+    idc.MakeName(ppayload + offset_of(section.data.payload, 'count'), 'globals_count')
+    MakeN(ppayload + offset_of(section.data.payload, 'count'), size_of(section.data.payload, 'count'))
+
+    pglobals = ppayload + offset_of(section.data.payload, 'globals')
+    pcur = pglobals
+    for i, body in enumerate(section.data.payload.globals):
+        gname = 'global_%X' % (i)
+
+        ptype = pcur + offset_of(body, 'type')
+        idc.MakeName(ptype + offset_of(body.type, 'content_type'), gname + '_content_type')
+        MakeN(ptype + offset_of(body.type, 'content_type'), size_of(body.type, 'content_type'))
+        ctype = idawasm.const.WASM_TYPE_NAMES[body.type.content_type]
+        idaapi.append_cmt(ptype + offset_of(body.type, 'content_type'), ctype, False)
+
+        idc.MakeName(ptype + offset_of(body.type, 'mutability'), gname + '_mutability')
+        MakeN(ptype+ offset_of(body.type, 'mutability'), size_of(body.type, 'mutability'))
+
+        # we need a target that people can rename.
+        # so lets map `global_N` to the init expr field.
+        # this will look like:
+        #
+        #     global_0        <---- named address we can reference
+        #     global_0_init:  <---- fake label line
+        #        i32.const    <---- init expression insns
+        #        ret
+        pinit = pcur + offset_of(body, 'init')
+        idc.MakeName(pinit, gname)
+        idc.ExtLinA(pinit, 0, gname + '_init:')
+        idc.MakeCode(pinit)
+
+        pcur += size_of(body)
+
+
+
 SECTION_LOADERS = {
     wasm.wasmtypes.SEC_CODE: load_code_section,
+    wasm.wasmtypes.SEC_GLOBAL: load_globals_section,
 }
+
+
+def compute_global_addrs(sections):
+    ret = []
+    section = get_section(sections, wasm.wasmtypes.SEC_GLOBAL)
+    ppayload = p + offset_of(section.data, 'payload')
+    pglobals = ppayload + offset_of(section.data.payload, 'globals')
+    pcur = pglobals
+    for i, body in enumerate(section.data.payload.globals):
+        ret.append(pcur + offset_of(body, 'init'))
+        pcur += size_of(body)
+
+
+def compute_function_addrs(sections):
+    ret = []
+    section = get_section(sections, wasm.wasmtypes.SEC_FUNCTION)
+    ppayload = p + offset_of(section.data, 'payload')
+    pbodies = ppayload + offset_of(section.data.payload, 'bodies')
+    pcur = pbodies
+    for i, body in enumerate(section.data.payload.bodies):
+        pcode = pcur + offset_of(body, 'code')
+        ret.append({
+            'index': i,
+            'addr': pcode,
+            'body': body,
+        })
+        pcur += size_of(body)
 
 
 def load_file(f, neflags, format):
